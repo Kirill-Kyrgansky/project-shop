91a5447d8f0f72578e1d68b38f949cc80adead13
kyrgansky.ku <kyrgansky.ku@zdmail.ru>
2023-06-13 16:24:31 +0300

    create footer and header

--- js/index.js
+++ js/index.js
@@ -1,7 +1,29 @@
-// $(function() {
-//     $('.marquee').marquee({
-//         duration: 7000,
-//         startVisible: true,
-//         duplicated: true
-//     });
-// });
+$(function () {
+    $('.marquee').marquee({
+        duration: 17000,
+        startVisible: true,
+        duplicated: true
+    });
+});
+
+
+function closeDropdown() {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+        const openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+        }
+    }
+}
+
+function showList(type) {
+    this.closeDropdown()
+    document.getElementById(type).classList.toggle("show");
+}
+
+closeWindow = window.onclick = function (event) {
+    if (!event.target.matches('.dropdown')) {
+        this.closeDropdown()
+    }
+}
